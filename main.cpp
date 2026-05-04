05f3c90dc9589d16129c64080fb491a6ecde2bac
StrikingKybra <145154946+StrikingKybra@users.noreply.github.com>
2026-05-04 10:53:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -168,11 +168,18 @@ void addGradedItem(vector<unique_ptr<GradedItem>>& items) {
     cout << "1. Homework\n";
     cout << "2. Quiz\n";
     cout << "3. Exam\n";
-    cout << "Choice: ";
-
     int category;
+    while (true){
+    cout << "Choice: ";
     cin >> category;
-    cin.ignore();
+    if (std::cin.fail() || category < 1 || category > 3) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "Invalid input. Please enter 1, 2, or 3 only.\n";
+        } else {
+            break;
+        }
+    }
 
     cout << "Name: ";
     string name;
